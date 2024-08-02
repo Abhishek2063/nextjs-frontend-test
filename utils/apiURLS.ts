6b34c145ccccf53129774aaaaa9156b053553113
abhishek <abhishekgarg2063@gmail.com>
2024-08-02 09:37:30 +0530

    added the material ui and emotion styled in the application

---
+++ utils/apiURLS.ts
@@ -0,0 +1,3 @@
+const base_url = process.env.NEXT_PUBLIC_API_URL || "https://dummyjson.com"
+export const LOGIN_API = `${base_url}/auth/login`
+export const PRODUCT_GET_LIST_API = `${base_url}/auth/login`
